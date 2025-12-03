a5f34f727bd681a57b14955ca3e6a6ab92861cf6
Harsh18patel <patelharsh503503@gmail.com>
2025-12-03 17:05:03 +0530

    cahnge in axios

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+// Auto switch URL based on environment
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:4000"
+    : "https://xtrack.xvantageinfotech.com/api";
+
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL,
 });
 
-// Attach token automatically
+// Attach token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("xtrack_token");
   if (token) {
